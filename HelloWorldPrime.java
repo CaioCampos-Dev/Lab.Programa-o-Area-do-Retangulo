c6bf706df49fe51a8793a21be24f47ed9b185361
CaioCampos-Dev <camposcaio889@gmail.com>
2026-05-08 21:41:01 -0300

    Add files via upload

---
+++ HelloWorldPrime.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class HelloWorldPrime{
+public static void main(String[] args){
+    Scanner ler = new Scanner(System.in);
+
+    System.out.print("Digite a base do retangulo: ");
+    int B = ler.nextInt();
+
+    System.out.print("Digite a altura do retangulo: ");
+    int H = ler.nextInt();
+
+    int resultado = B*H;
+
+    System.out.println("Resultado" + resultado);
+}
+}
