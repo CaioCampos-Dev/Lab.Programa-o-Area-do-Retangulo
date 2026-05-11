d3db4cc5d8364bf27eadbf879163879ba8e47dc4
CaioCampos-Dev <camposcaio889@gmail.com>
2026-05-11 11:20:29 -0300

    Add files via upload

---
+++ AreadoCubo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class AreadoCubo{
+public static void main(String[] args){
+    Scanner ler = new Scanner(System.in);
+
+    double Z = 3.14;
+
+    System.out.print("Digite a aresta do cubo: ");
+    int A = ler.nextInt();
+
+    double Base = A*A;
+
+    System.out.println("Area da base " + Base);
+
+    double Lateral = 4*(A*A);
+
+    System.out.println("Area lateral " + Lateral);
+
+    double total = 6*(A*A);
+
+    System.out.println("Area total " + total);
+}
+}
